aefac838d8aab0e2de591a2d07b7c1febb962ac2
Yijun Gai <yijun@i-motor.com.au>
2019-04-02 12:17:59 +1100

    Updates

---
+++ precache-manifest.861e6576ac93c9f38ea3b8bcbf0eef7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/usyd-cds/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d956da6e26a53587129e",
+    "url": "/usyd-cds/static/js/runtime~main.6c33dc50.js"
+  },
+  {
+    "revision": "87d59a54639ded6cf261",
+    "url": "/usyd-cds/static/js/main.83a4677f.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/usyd-cds/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "87d59a54639ded6cf261",
+    "url": "/usyd-cds/static/css/main.694f2cf1.chunk.css"
+  },
+  {
+    "revision": "9d100616dfb6d64553d0029cea6bba09",
+    "url": "/usyd-cds/index.html"
+  }
+];
